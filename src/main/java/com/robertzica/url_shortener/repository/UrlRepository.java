1faa99d91068d0a3ecefba1c71406d5f70166985
randontime <randondesigner19@gmail.com>
2026-05-26 23:16:49 -0300

    feat: adiciona UrlRepository com JpaRepository e busca por codigo

---
+++ src/main/java/com/robertzica/url_shortener/repository/UrlRepository.java
@@ -0,0 +1,8 @@
+package com.robertzica.url_shortener.repository;
+
+import com.robertzica.url_shortener.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UrlRepository extends JpaRepository<Url, Long> {
+    Url findByCodigo(String codigo);
+}
