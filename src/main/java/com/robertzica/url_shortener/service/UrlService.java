7d8b8e280483b121457c575c34dea1e2d9587db0
randontime <randondesigner19@gmail.com>
2026-05-26 23:29:23 -0300

    feat: adiciona UrlService com lógica de encurtamento e geração de código aleatório

---
+++ src/main/java/com/robertzica/url_shortener/service/UrlService.java
@@ -0,0 +1,25 @@
+package com.robertzica.url_shortener.service;
+
+import com.robertzica.url_shortener.model.Url;
+import com.robertzica.url_shortener.repository.UrlRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class UrlService {
+
+    @Autowired
+    private UrlRepository repository;
+
+    public Url encurtarUrl(String urlOriginal) {
+        Url url = new Url();
+        url.setUrlOriginal(urlOriginal);
+        url.setCodigo(UUID.randomUUID().toString().substring(0, 6));
+        url.setCriadoEm(LocalDateTime.now());
+        url.setAcessos(0L);
+        return repository.save(url);
+    }
+}
