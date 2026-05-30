b3508ac72b516cffec5026ff7dfb9c98ce52569e
randontime <randondesigner19@gmail.com>
2026-05-30 00:02:12 -0300

    feat: adiciona método de geração de QR Code no UrlService

--- src/main/java/com/robertzica/url_shortener/service/UrlService.java
+++ src/main/java/com/robertzica/url_shortener/service/UrlService.java
@@ -1,10 +1,17 @@
 package com.robertzica.url_shortener.service;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.encoder.QRCode;
 import com.robertzica.url_shortener.model.Url;
 import com.robertzica.url_shortener.repository.UrlRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -27,5 +34,18 @@ public class UrlService {
         Url url = repository.findByCodigo(codigo);
         url.setAcessos(url.getAcessos() + 1);
         return repository.save(url);
+    }
+
+    public byte[] gerarQrCode(String codigo) throws Exception {
+        Url url = repository.findByCodigo(codigo);
+        String urlCurta = "http://localhost:8080/" + codigo;
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(urlCurta, BarcodeFormat.QR_CODE, 300, 300);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+
+        return outputStream.toByteArray();
     }
 }
